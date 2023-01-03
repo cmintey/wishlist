ac41b5523b403381731feab6cba7926724fb3323
Carter Mintey <cmintey8@gmail.com>
2023-01-03 13:08:09 -0600

    added stuff for pwa with dummy images

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import type { UserConfig } from "vite";
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Wishlist",
+				short_name: "Wishlist",
+				description: "Christmas wishlist you can share with the whole family.",
+				theme_color: "#fff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png"
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png"
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable"
+					}
+				]
+			}
+		})
+	],
 	server: {
 		fs: {
 			// Allow serving files from one level up to the project root
