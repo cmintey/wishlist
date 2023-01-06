2584d4b43b8ba66b3fc5ec18fc21dd79fc627341
Carter Mintey <cmintey8@gmail.com>
2023-01-06 11:53:23 -0600

    improved the look of the login/signup/password-reset forms

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,73 +1,90 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
-	import { focusTrap } from "@skeletonlabs/skeleton";
 	import type { ActionData, PageServerData } from "./$types";
 
 	export let data: PageServerData;
 	export let form: ActionData;
-	let password = "";
+
+	let pwdVisible = false;
+	const handleClick = () => {
+		pwdVisible = !pwdVisible;
+	};
 </script>
 
-<div class="h-full">
-	<div class="flex flex-col space-y-4 items-center content-center">
-		<h1>Sign in</h1>
+<div class="flex flex-col space-y-4 items-center">
+	<h1>Sign in</h1>
 
-		<form
-			method="POST"
-			use:enhance={() => {
-				return async ({ result, update }) => {
-					if (result.type === "failure") {
-						password = "";
-					}
-					await update();
-				};
-			}}
+	<form
+		class="w-80"
+		method="POST"
+		use:enhance={(form) => {
+			return async ({ result, update }) => {
+				if (result.type === "failure") {
+					form.form.reset();
+				}
+				await update();
+			};
+		}}
+	>
+		<div
+			class="flex flex-col space-y-4 p-4 rounded-container-token bg-surface-100-800-token ring-outline-token"
 		>
-			<div
-				class="flex flex-col space-y-4 p-4 rounded-container-token bg-surface-100-800-token ring-outline-token"
-				use:focusTrap={true}
-			>
-				<label for="username" class="w-fit">
-					<span>Username</span>
-					<input
-						type="text"
-						id="username"
-						name="username"
-						autocomplete="username"
-						autocapitalize="off"
-						required
-					/>
+			<label for="username">
+				<span>Username</span>
+				<input
+					type="text"
+					id="username"
+					name="username"
+					autocomplete="username"
+					autocapitalize="off"
+					required
+				/>
+			</label>
+
+			<div class="flex flex-col space-y-4 relative">
+				<label for="password">
+					<span>Password</span>
+					<div class="relative">
+						<input
+							type={pwdVisible ? "text" : "password"}
+							id="password"
+							name="password"
+							class="pr-8"
+							required
+						/>
+						<button
+							class="btn-icon-sm flex absolute inset-y-0 right-0 items-center mt-1.5 pr-4 z-10"
+							type="button"
+							id="showpassword"
+							on:click|preventDefault={handleClick}
+							on:keypress|preventDefault
+						>
+							<iconify-icon icon="ri:{pwdVisible ? 'eye-off-fill' : 'eye-fill'}" />
+						</button>
+					</div>
 				</label>
 
-				<div class="flex flex-col space-y-4 relative">
-					<label for="password" class="w-fit">
-						<span>Password</span>
-						<input type="password" id="password" name="password" bind:value={password} required />
-					</label>
+				{#if form?.incorrect}<span class="unstyled text-xs text-red-500">Invalid credentials!</span
+					>{/if}
+				{#if form?.error}
+					<ul>
+						{#each form.errors as error}
+							<li class="text-xs text-red-500">{error.message}</li>
+						{/each}
+					</ul>
+				{/if}
 
-					{#if form?.incorrect}<span class="unstyled text-xs text-red-500"
-							>Invalid credentials!</span
-						>{/if}
-					{#if form?.error}
-						<ul>
-							{#each form.errors as error}
-								<li class="text-xs text-red-500">{error.message}</li>
-							{/each}
-						</ul>
+				<div class="flex space-x-4 justify-center items-center">
+					<button class="btn btn-filled-primary w-min">Log In</button>
+					{#if data.enableSignup}
+						<a href="/signup">Create an account</a>
 					{/if}
+				</div>
 
-					<div class="flex space-x-4 justify-center items-center">
-						<button class="btn btn-filled-primary w-min">Log In</button>
-						{#if data.enableSignup}
-							<a href="/signup">Create an account</a>
-						{/if}
-					</div>
-
-					<div>
-						<a href="/forgot-password" class="text-sm absolute top-0 right-0">Forgot password?</a>
-					</div>
+				<div>
+					<a href="/forgot-password" class="text-sm absolute top-0 right-0">Forgot password?</a>
 				</div>
 			</div>
-		</form>
-	</div>
+		</div>
+	</form>
 </div>
