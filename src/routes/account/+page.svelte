ed40e8086d4cfbea6165d00fda41a65c27dcf5bf
Carter <35710697+cmintey@users.noreply.github.com>
2023-03-07 10:44:36 -0600

    UI updates (#37)
    * update item card image and link open in new tab
    
    * fix profile picture button on mobile
    
    * update workflow
    
    * exclude .github folder from linting

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -19,14 +19,19 @@
 	<svelte:fragment slot="panel">
 		{#if tabSet === 0}
 			<div class="flex flex-col w-fit items-center">
-				<div class="picture">
-					<Avatar user={data.user} width="w-24 md:w-32" />
-					<form method="POST" class="add-icon" action="?/profilePicture" use:enhance>
+				<div class="relative w-full h-full m-auto max-w-[150px]">
+					<Avatar user={data.user} width="w-32" />
+					<form
+						method="POST"
+						class="absolute w-12 h-12 bottom-0 right-0"
+						action="?/profilePicture"
+						use:enhance
+					>
 						<FileButton
 							name="profilePic"
 							id="profilePic"
 							accept="image/*"
-							button="btn-icon variant-glass-secondary"
+							button="btn-icon btn-icon-sm variant-glass-secondary"
 							on:change={() => submitButton.click()}
 						>
 							<iconify-icon icon="ion:camera" class="text-2xl" />
@@ -46,23 +51,3 @@
 <svelte:head>
 	<title>Account</title>
 </svelte:head>
-
-<style>
-	.picture {
-		width: 100% !important;
-		height: 100% !important;
-		max-width: 150px !important;
-		max-height: 150px !important;
-		margin: auto;
-		position: relative;
-	}
-
-	.add-icon {
-		width: 50px;
-		height: 50px;
-		border-radius: 100%;
-		position: absolute;
-		bottom: 0;
-		right: 0;
-	}
-</style>
