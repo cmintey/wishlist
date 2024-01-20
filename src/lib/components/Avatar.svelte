43e7bddce751f7d08f1327123cef121bc9db8af7
Carter Mintey <cmintey8@gmail.com>
2024-01-20 14:27:50 -0600

    add height style to avatar

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -8,8 +8,9 @@
 </script>
 
 <Avatar
+    style="height:100%"
     background="bg-primary-400-500-token"
     initials={user?.name.split(" ").reduce((x, y) => x + y.at(0), "")}
-    src={user?.picture ? `/api/assets/${user.picture}` : ""}
+    src={`/api/assets/${user.picture}`}
     {...$$props}
 />
