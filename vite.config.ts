87708a3195be55b55c37febd525a42991039bcf3
Carter <35710697+cmintey@users.noreply.github.com>
2024-01-28 17:13:39 -0600

    add about page with settings and build information (#105)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import { exec } from "child_process";
+import { promisify } from "util";
 import type { UserConfig } from "vite";
 import { purgeCss } from "vite-plugin-tailwind-purgecss";
+
+// Get current tag/commit and last commit date from git
+const pexec = promisify(exec);
+const [version, sha] = (
+    await Promise.all([pexec("git describe --tags || git rev-parse --short HEAD"), pexec("git rev-parse --short HEAD")])
+).map((v) => JSON.stringify(v?.stdout.trim()));
 
 const config: UserConfig = {
     plugins: [
@@ -45,6 +53,11 @@ const config: UserConfig = {
             // Allow serving files from one level up to the project root
             allow: ["./static/"]
         }
+    },
+    define: {
+        __VERSION__: version,
+        __COMMIT_SHA__: sha,
+        __LASTMOD__: Date.now()
     }
 };
 
