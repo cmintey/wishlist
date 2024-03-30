094160da3be9103e079a6e8670199e5711bb8eea
Carter Mintey <cmintey8@gmail.com>
2024-03-29 23:13:56 -0500

    fix migration

--- prisma/migrations/20240330024912_standardize_session_casing/migration.sql
+++ prisma/migrations/20240330024912_standardize_session_casing/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `user_id` on the `session` table. All the data in the column will be lost.
-  - Added the required column `userId` to the `session` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_session" (
@@ -13,7 +6,7 @@ CREATE TABLE "new_session" (
     "expiresAt" DATETIME NOT NULL,
     CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_session" ("expiresAt", "id") SELECT "expiresAt", "id" FROM "session";
+INSERT INTO "new_session" ("expiresAt", "id", "userId") SELECT "expiresAt", "id", "user_id" FROM "session";
 DROP TABLE "session";
 ALTER TABLE "new_session" RENAME TO "session";
 CREATE UNIQUE INDEX "session_id_key" ON "session"("id");
