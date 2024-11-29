21dc47831fd507ec79c34e2a19ac60e9573bb376
Albin Medoc <stamline203@gmail.com>
2024-11-29 14:45:44 +0100

    Add header authentication

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,23 +1,80 @@
 import { fail, redirect } from "@sveltejs/kit";
+import { env } from "$env/dynamic/private";
 import { auth } from "$lib/server/auth";
-import type { PageServerLoad, Actions } from "./$types";
 import { loginSchema } from "$lib/validations";
 import { getConfig } from "$lib/server/config";
+import { Role } from "$lib/schema";
 import { client } from "$lib/server/prisma";
 import { LegacyScrypt } from "lucia";
+import type { PageServerLoad, Actions } from "./$types";
 
-export const load: PageServerLoad = async ({ locals, request }) => {
+export const load: PageServerLoad = async ({ locals, request, cookies }) => {
+    const config = await getConfig();
+    const ref = new URL(request.url).searchParams.get("ref");
     if (locals.user) {
-        const ref = new URL(request.url).searchParams.get("ref");
         redirect(302, ref || "/");
     }
+
+    /* Header authentication */
+    const username = env.HEADER_USERNAME ? request.headers.get(env.HEADER_USERNAME) : null;
+    if (username) {
+        let user = await client.user.findUnique({
+            select: {
+                id: true,
+            },
+            where: {
+                username: username
+            }
+        });
+
+        if (!user) {
+            if(!env.HEADER_EMAIL || !env.HEADER_NAME) {
+                return fail(400, { username: username, password: "", incorrect: true });
+            }
+            const name = request.headers.get(env.HEADER_NAME);
+            const email = request.headers.get(env.HEADER_EMAIL);
+            if(!name || !email) {
+                return fail(400, { username: username, password: "", incorrect: true });
+            }
+            const userCount = await client.user.count();
+            user = await client.user.create({
+                select: {
+                    id: true
+                },
+                data: {
+                    username: username,
+                    email: email,
+                    hashedPassword: "",
+                    name: name,
+                    roleId: userCount > 0 ? Role.USER : Role.ADMIN
+                }
+            });
+            if (config.defaultGroup) {
+                await client.userGroupMembership.create({
+                    data: {
+                        groupId: config.defaultGroup,
+                        userId: user.id,
+                        active: true
+                    }
+                });
+            }
+        }
+
+        const session = await auth.createSession(user.id, {});
+        const sessionCookie = auth.createSessionCookie(session.id);
+        cookies.set(sessionCookie.name, sessionCookie.value, {
+            path: "/",
+            ...sessionCookie.attributes
+        });
+        redirect(302, ref || "/");
+    }
+    /* End header authentication */
 
     const userCount = await client.user.count();
     if (userCount === 0) {
         redirect(302, "/setup-wizard");
     }
 
-    const config = await getConfig();
     return { enableSignup: config.enableSignup };
 };
 
