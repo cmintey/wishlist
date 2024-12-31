477fa929219c6f3c52d7c91acec8281a7b579140
Carter Mintey <cmintey8@gmail.com>
2024-12-31 16:52:54 -0600

    fix import

--- src/routes/items/[itemId]/edit/+page.server.ts
+++ src/routes/items/[itemId]/edit/+page.server.ts
@@ -1,5 +1,5 @@
 import { error, fail, redirect } from "@sveltejs/kit";
-import type { Actions, PageServerLoad } from "../$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { client } from "$lib/server/prisma";
 import { getConfig } from "$lib/server/config";
 import { getActiveMembership } from "$lib/server/group-membership";
