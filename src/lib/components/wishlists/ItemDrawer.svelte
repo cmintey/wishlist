ee476bf38808e73e7fd6ceffe5c3d8f44759a5b4
Carter Mintey <cmintey8@gmail.com>
2025-03-08 19:37:11 -0600

    tailwind v4

--- src/lib/components/wishlists/ItemDrawer.svelte
+++ src/lib/components/wishlists/ItemDrawer.svelte
@@ -37,7 +37,7 @@
 
 <div class="flex max-h-[80dvh] flex-col space-y-2 p-4 pb-4">
     <div class="flex w-full justify-center">
-        <hr class="self-c !w-12 !rounded !border-t-4" />
+        <hr class="self-c w-12! rounded-sm! border-t-4!" />
     </div>
     <span class="text-wrap break-words text-xl font-bold md:text-2xl">
         {item.name}
--- src/lib/components/wishlists/ItemForm.svelte
+++ src/lib/components/wishlists/ItemForm.svelte
@@ -279,7 +279,7 @@
                         type="checkbox"
                         value={list.id}
                     />
-                    <div class="!mt-0 flex flex-row space-x-2">
+                    <div class="mt-0! flex flex-row space-x-2">
                         <span>
                             {list.name}
                         </span>
