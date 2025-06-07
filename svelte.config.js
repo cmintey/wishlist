f164667401b3dd4ef2e2fd7d36e6a1a7949a1109
Carter Mintey <cmintey8@gmail.com>
2025-06-07 17:47:20 -0500

    use melt ui for modal

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import adapter from "@sveltejs/adapter-node";
 import { sveltePreprocess } from "svelte-preprocess";
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: [
+    preprocess: sequence([
         sveltePreprocess({
             postcss: true
-        })
-    ],
+        }),
+        preprocessMeltUI()
+    ]),
 
     kit: {
         adapter: adapter()
