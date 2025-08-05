a145ae192f81cc7188377913400e193f2686db6a
Carter Mintey <cmintey8@gmail.com>
2025-08-04 20:34:28 -0500

    cleanup

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -12,14 +12,3 @@ export const trimToNull = (s: string | null) => {
 export const rgbToHex = (r: number, g: number, b: number) => {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 };
-
-export const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) => groupByAndMap(arr, key, (a) => a);
-
-export const groupByAndMap = <T, K extends keyof any, M>(arr: T[], key: (i: T) => K, mapper: (t: T) => M) =>
-    arr.reduce(
-        (groups, item) => {
-            (groups[key(item)] ||= []).push(mapper(item));
-            return groups;
-        },
-        {} as Record<K, M[]>
-    );
