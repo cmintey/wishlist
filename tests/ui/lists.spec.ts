66bb502f3561855146c289fd9b91db1adb94cddc
Carter Mintey <cmintey8@gmail.com>
2025-08-05 17:09:51 -0500

    streamline user creation

--- tests/ui/lists.spec.ts
+++ tests/ui/lists.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "../fixtures";
 import { ListsPage } from "../pageObjects/lists.page";
-import { addUserToGroup, createUser, randomString } from "../util";
+import { randomString } from "../util";
 
 test("create list with default values", async ({ page, userData }) => {
     const listsPage = new ListsPage(page);
@@ -77,26 +77,28 @@ test("delete list", async ({ page }) => {
     await listsPage.expectListCount(countBefore);
 });
 
-test("multiple users and list filter", async ({ page: user1Page, browser, userData }) => {
+test("multiple users and list filter", async ({
+    page: user1Page,
+    userData,
+    additionalPage: user2Page,
+    additionalUserData: user2
+}) => {
     const user1ListsPage = new ListsPage(user1Page);
     await user1ListsPage.goto();
 
-    await user1ListsPage.expectListCount(1);
     const user1List = await user1ListsPage.getListAt(0);
     await user1List.assertOwner(userData.name);
 
-    const user2 = await createUser(browser);
-    await addUserToGroup(user1Page, user2.getUserData().name);
-    const user2ListsPage = new ListsPage(user2.getPage());
+    const user2ListsPage = new ListsPage(user2Page);
     await user2ListsPage.goto();
     const user2List = await user2ListsPage.getListAt(0);
-    await user2List.assertOwner(user2.getUserData().name);
+    await user2List.assertOwner(user2.name);
 
     await user1ListsPage.expectListCount(2);
     await user2ListsPage.expectListCount(2);
 
     // Assert second list from User 1's perspective is User 2's list
-    await user1ListsPage.getListAt(1).then((list) => list.assertOwner(user2.getUserData().name));
+    await user1ListsPage.getListAt(1).then((list) => list.assertOwner(user2.name));
     // Assert second list from User 2's perspective is User 1's list
     await user2ListsPage.getListAt(1).then((list) => list.assertOwner(userData.name));
 
@@ -109,9 +111,7 @@ test("multiple users and list filter", async ({ page: user1Page, browser, userDa
     await user1ListsPage.expectListCount(2);
 
     // Filter by user 2 lists
-    await user1ListsPage.filterLists(user2.getUserData().name);
+    await user1ListsPage.filterLists(user2.name);
     await user1ListsPage.expectListCount(1);
-    await user1ListsPage.getListAt(0).then((list) => list.assertOwner(user2.getUserData().name));
-
-    await user2.cleanup();
+    await user1ListsPage.getListAt(0).then((list) => list.assertOwner(user2.name));
 });
