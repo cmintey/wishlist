cb62beb4454bc9319f93fefb81c3a6379214bfdb
Carter Mintey <cmintey8@gmail.com>
2025-08-04 20:32:40 -0500

    remove modal bit

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
 import adapter from "@sveltejs/adapter-node";
 import { sveltePreprocess } from "svelte-preprocess";
-import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: sequence([
-        sveltePreprocess({
-            postcss: true
-        }),
-        preprocessMeltUI()
-    ]),
+    preprocess: sveltePreprocess({
+        postcss: true
+    }),
 
     kit: {
         adapter: adapter()
