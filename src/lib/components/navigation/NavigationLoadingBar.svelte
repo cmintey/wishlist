8348b90e2d41ed61340625f9c06caf81d48916f5
Carter Mintey <cmintey8@gmail.com>
2025-11-17 21:39:37 -0600

    add some delay to navigation bar for fast page loads

--- src/lib/components/navigation/NavigationLoadingBar.svelte
+++ src/lib/components/navigation/NavigationLoadingBar.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { cubicOut } from "svelte/easing";
-    import { tweened } from "svelte/motion";
-    const progress = tweened(0, {
+    import { Tween } from "svelte/motion";
+
+    const delay = 100;
+    const progress = new Tween(0, {
         duration: 1000,
         easing: cubicOut
     });
+
     onMount(() => {
-        progress.set(90);
+        const timer = setTimeout(() => {
+            progress.set(90);
+        }, delay);
+        return () => clearTimeout(timer);
     });
 </script>
 
-<div class="absolute left-0 top-0 z-[999999999] h-[3px] w-screen bg-white">
-    <span style:width={`${$progress}%`} class="bg-surface-backdrop-token absolute h-[3px]"></span>
-</div>
+{#if progress.current}
+    <div class="absolute left-0 top-0 z-[999999999] h-[3px] w-screen bg-white">
+        <span style:width={`${progress.current}%`} class="bg-surface-backdrop-token absolute h-[3px]"></span>
+    </div>
+{/if}
