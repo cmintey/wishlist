7c865b59ebc90a994c98ff3c02d1089bac5a8bc5
juzim <julianzimmermann@gmail.com>
2025-11-24 00:24:49 +0100

    feat: Add archive feature (#226) migrations should not not drop the table

--- prisma/migrations/20251123182820_change_archived_boolean_to_archived_by_string/migration.sql
+++ prisma/migrations/20251123182820_change_archived_boolean_to_archived_by_string/migration.sql
@@ -4,31 +4,8 @@
   - You are about to drop the column `archived` on the `items` table. All the data in the column will be lost.
 
 */
--- RedefineTables
-PRAGMA defer_foreign_keys=ON;
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_items" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "price" TEXT,
-    "itemPriceId" TEXT,
-    "url" TEXT,
-    "note" TEXT,
-    "imageUrl" TEXT,
-    "quantity" INTEGER,
-    "archivedById" TEXT,
-    "userId" TEXT NOT NULL,
-    "createdById" TEXT NOT NULL,
-    CONSTRAINT "items_itemPriceId_fkey" FOREIGN KEY ("itemPriceId") REFERENCES "item_price" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "items_archivedById_fkey" FOREIGN KEY ("archivedById") REFERENCES "user" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "items_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "items_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-INSERT INTO "new_items" ("createdById", "id", "imageUrl", "itemPriceId", "name", "note", "price", "quantity", "url", "userId") SELECT "createdById", "id", "imageUrl", "itemPriceId", "name", "note", "price", "quantity", "url", "userId" FROM "items";
-DROP TABLE "items";
-ALTER TABLE "new_items" RENAME TO "items";
-CREATE UNIQUE INDEX "items_id_key" ON "items"("id");
-CREATE INDEX "items_userId_idx" ON "items"("userId");
+-- DropColumn and AddColumn
+ALTER TABLE "items" DROP COLUMN "archived";
+ALTER TABLE "items" ADD COLUMN "archivedById" TEXT;
 CREATE INDEX "items_archivedById_idx" ON "items"("archivedById");
-PRAGMA foreign_keys=ON;
-PRAGMA defer_foreign_keys=OFF;
+ALTER TABLE "items" ADD CONSTRAINT "items_archivedById_fkey" FOREIGN KEY ("archivedById") REFERENCES "user" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
