191b3f1becebfa50c370a21a9a82ba51225d21ea
Carter Mintey <cmintey8@gmail.com>
2025-12-08 22:16:50 -0600

    add default to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ export PROTOCOL_HEADER=x-forwarded-proto
 export HOST_HEADER=x-forwarded-host
 export DATABASE_URL="file:/usr/src/app/data/prod.db"
 export PUBLIC_DEFAULT_CURRENCY=${DEFAULT_CURRENCY}
-export BODY_SIZE_LIMIT=${MAX_IMAGE_SIZE}
+export BODY_SIZE_LIMIT=${MAX_IMAGE_SIZE:-5000000}
 
 caddy start --config /usr/src/app/Caddyfile
 
