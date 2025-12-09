026d93d720134bcda9eb9d0025bc61f2770fc6df
Carter Mintey <cmintey8@gmail.com>
2025-12-08 21:20:21 -0600

    default url

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
         seed: "tsx prisma/seed.ts"
     },
     datasource: {
-        url: env("DATABASE_URL")
+        url: env("DATABASE_URL") || "prisma/dev.db"
     }
 });
