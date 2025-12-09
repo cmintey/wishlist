740c334963fde1e509f3526c8b2d6848c6462ab7
Carter Mintey <cmintey8@gmail.com>
2025-12-08 21:22:59 -0600

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
@@ -8,6 +8,6 @@ export default defineConfig({
         seed: "tsx prisma/seed.ts"
     },
     datasource: {
-        url: env("DATABASE_URL") || "prisma/dev.db"
+        url: process.env.DATABASE_URL || "prisma/dev.db"
     }
 });
