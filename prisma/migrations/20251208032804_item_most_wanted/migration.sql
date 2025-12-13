cf05df5850aaaec1db6e53f55db385ab0d0ed7f1
Carter <35710697+cmintey@users.noreply.github.com>
2025-12-13 12:14:41 -0600

    feat: better re-ordering capabilities and "most wanted" items (#556)
    * create badge
    
    * allow display order to be edited directly
    
    * create item with displayOrder populated
    
    * persist most wanted boolean
    
    * move most wanted items to the top of list
    
    * fix bugs
    
    * tests

---
+++ prisma/migrations/20251208032804_item_most_wanted/migration.sql
@@ -0,0 +1,26 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_items" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" TEXT,
+    "itemPriceId" TEXT,
+    "url" TEXT,
+    "note" TEXT,
+    "imageUrl" TEXT,
+    "quantity" INTEGER,
+    "mostWanted" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "createdById" TEXT NOT NULL,
+    CONSTRAINT "items_itemPriceId_fkey" FOREIGN KEY ("itemPriceId") REFERENCES "item_price" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "items_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "items_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_items" ("createdById", "id", "imageUrl", "itemPriceId", "name", "note", "price", "quantity", "url", "userId") SELECT "createdById", "id", "imageUrl", "itemPriceId", "name", "note", "price", "quantity", "url", "userId" FROM "items";
+DROP TABLE "items";
+ALTER TABLE "new_items" RENAME TO "items";
+CREATE UNIQUE INDEX "items_id_key" ON "items"("id");
+CREATE INDEX "items_userId_idx" ON "items"("userId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
