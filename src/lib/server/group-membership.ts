f0039aba6cac02c72df0f10d72360d6455cef51f
Carter <35710697+cmintey@users.noreply.github.com>
2025-12-19 11:16:48 -0600

    feat: "share to Wishlist" share target for installed PWA (#553)
    * add to manifest
    
    * add an import page
    
    * fix import

--- src/lib/server/group-membership.ts
+++ src/lib/server/group-membership.ts
@@ -1,5 +1,7 @@
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 import { client } from "./prisma";
+import type { UserGroupMembership } from "$lib/generated/prisma/client";
+import { getFormatter } from "./i18n";
 
 export const getActiveMembership = async (user: LocalUser) => {
     const activeMembership = await client.userGroupMembership.findFirst({
@@ -29,4 +31,52 @@ export const getActiveMembership = async (user: LocalUser) => {
     }
 
     return activeMembership;
+};
+
+export const setActiveMembership = async (userId: string, groupId: string) => {
+    const $t = await getFormatter();
+
+    const activeMembership = await client.userGroupMembership.findFirst({
+        where: {
+            userId,
+            active: true
+        }
+    });
+
+    let membership: UserGroupMembership;
+    try {
+        membership = await client.userGroupMembership.findFirstOrThrow({
+            where: {
+                groupId,
+                userId
+            }
+        });
+    } catch {
+        error(400, $t("errors.user-is-not-a-member-of-the-group"));
+    }
+
+    if (activeMembership?.id === membership.id) {
+        return membership;
+    }
+
+    membership = await client.userGroupMembership.update({
+        where: {
+            id: membership.id
+        },
+        data: {
+            active: true
+        }
+    });
+
+    if (activeMembership) {
+        await client.userGroupMembership.update({
+            where: {
+                id: activeMembership.id
+            },
+            data: {
+                active: false
+            }
+        });
+    }
+    return membership;
 };
--- src/routes/api/users/[userId]/groups/[groupId]/+server.ts
+++ src/routes/api/users/[userId]/groups/[groupId]/+server.ts
@@ -1,10 +1,9 @@
 import { Role } from "$lib/schema";
-import { client } from "$lib/server/prisma";
-import type { UserGroupMembership } from "$lib/generated/prisma/client";
 import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import { getFormatter } from "$lib/server/i18n";
 import { requireLoginOrError } from "$lib/server/auth";
+import { setActiveMembership } from "$lib/server/group-membership";
 
 export const PATCH: RequestHandler = async ({ params, request }) => {
     const user = await requireLoginOrError();
@@ -16,46 +15,7 @@ export const PATCH: RequestHandler = async ({ params, request }) => {
     const data = await request.json();
 
     if (data.active) {
-        const activeMembership = await client.userGroupMembership.findFirst({
-            where: {
-                userId: params.userId,
-                active: true
-            }
-        });
-
-        let membership: UserGroupMembership;
-        try {
-            membership = await client.userGroupMembership.findFirstOrThrow({
-                where: {
-                    groupId: params.groupId,
-                    userId: params.userId
-                }
-            });
-        } catch {
-            error(400, $t("errors.user-is-not-a-member-of-the-group"));
-        }
-
-        if (activeMembership?.id === membership.id) error(400, $t("errors.group-is-already-active"));
-
-        membership = await client.userGroupMembership.update({
-            where: {
-                id: membership.id
-            },
-            data: {
-                active: true
-            }
-        });
-
-        if (activeMembership) {
-            await client.userGroupMembership.update({
-                where: {
-                    id: activeMembership.id
-                },
-                data: {
-                    active: false
-                }
-            });
-        }
+        const membership = await setActiveMembership(params.userId, params.groupId);
 
         return new Response(JSON.stringify({ membership }));
     }
