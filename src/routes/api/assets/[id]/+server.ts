5c25aa15df82e219d163bccc17ba78beed71aa9e
Carter Mintey <cmintey8@gmail.com>
2026-09-18 21:34:33 -0500

    prevent path traversal

--- src/routes/api/assets/[id]/+server.ts
+++ src/routes/api/assets/[id]/+server.ts
@@ -1,6 +1,7 @@
 import { getFormatter } from "$lib/server/i18n";
 import { error, type RequestHandler } from "@sveltejs/kit";
 import { readFileSync } from "fs";
+import path from "path";
 
 export const GET: RequestHandler = async ({ params }) => {
     const $t = await getFormatter();
@@ -9,8 +10,20 @@ export const GET: RequestHandler = async ({ params }) => {
         error(400, $t("errors.must-specify-asset-id"));
     }
 
+    const normalizedPath = path.normalize(params.id);
+    if (path.isAbsolute(normalizedPath)) {
+        error(400, $t("error.invalid-asset-path"));
+    }
+
+    const baseUploadsPath = path.resolve("uploads/");
+    const resolvedPath = path.resolve("uploads/" + normalizedPath);
+
+    if (!resolvedPath?.startsWith(baseUploadsPath)) {
+        error(400, $t("error.invalid-asset-path"));
+    }
+
     try {
-        const asset = readFileSync(`uploads/${params.id}`);
+        const asset = readFileSync(resolvedPath);
         return new Response(asset, {
             headers: {
                 "Cache-Control": "public, max-age=31536000"
