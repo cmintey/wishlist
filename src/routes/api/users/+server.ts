62e02dfb2386c8e865b36a5f70dbbdb7fae6268f
Carter <35710697+cmintey@users.noreply.github.com>
2026-09-20 21:38:52 -0500

    add additional checks on various APIs (#778)
    * add additional checks on various APIs
    
    * fix strings
    
    * use active group for role check

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -1,5 +1,8 @@
 import { Role } from "$lib/schema";
 import { requireLoginOrError, requireRole } from "$lib/server/auth";
+import { getActiveMembership } from "$lib/server/group-membership";
+import { getFormatter } from "$lib/server/i18n";
+import { logger } from "$lib/server/logger";
 import { client } from "$lib/server/prisma";
 import { createUser } from "$lib/server/user";
 import { getSignupSchema } from "$lib/server/validations";
@@ -7,7 +10,28 @@ import { error, type RequestHandler } from "@sveltejs/kit";
 import { treeifyError } from "zod";
 
 export const GET: RequestHandler = async ({ url }) => {
-    await requireLoginOrError();
+    const loggedInUser = await requireLoginOrError();
+    const activeGroup = await getActiveMembership(loggedInUser);
+
+    const $t = await getFormatter();
+
+    if (
+        !url.searchParams.has("groupId") &&
+        !(loggedInUser.roleId === Role.ADMIN || activeGroup.roleId === Role.GROUP_MANAGER)
+    ) {
+        logger.error(
+            { userId: loggedInUser.id, role: loggedInUser.roleId },
+            "User tried to list all users but is not an admin or group manager"
+        );
+        error(403, $t("errors.not-authorized"));
+    }
+
+    if (url.searchParams.has("groupId") && loggedInUser.roleId === Role.USER) {
+        if (activeGroup.groupId !== url.searchParams.get("groupId")) {
+            logger.error({ userId: loggedInUser.id }, "User tried to list users in a group they are not part of");
+            error(403, $t("errors.not-authorized"));
+        }
+    }
 
     const users = await client.user.findMany({
         select: {
